2b0841570691912e8cce2c65e5a938bc97a8ebbb
adamtsovartem <aay1990@gmail.com>
2014-03-03 22:07:40 +0400

    add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+  def after_sign_in_path_for(administrator)
+    if administrator.email != "admin@admin.ru" 
+      root_path
+    else
+	    admin_dashboard_index_path
+	  end
+  end
+private
+	  def layout_by_resource
+    	if devise_controller?
+        "admin"
+    	else
+    		"application"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 RailsShop::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  devise_for :administrators
+  devise_scope :administrators do
+    root to: "admin/dashboard#index" 
+  end
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
